87bd188240dc6b88deb58a380d8975ecc9448f3d
lagunovLev <lagunovl255@gmail.com>
2025-01-23 20:09:20 +0400

    letsgo

--- bot/__init__.py
+++ bot/__init__.py
@@ -191,7 +191,8 @@ def update_place():
 def webhook():
     print("Processing updates")
     flask.flash("Processing updates")
-    update = telebot.types.Update.de_json("")
+    json_string = flask.request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return '!', 200
 
