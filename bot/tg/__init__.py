8249262555d3f6d06adf5235b0a22fc3460438df
lagunovLev <lagunovl255@gmail.com>
2025-01-26 18:28:27 +0400

    rewrite

--- bot/tg/__init__.py
+++ bot/tg/__init__.py
@@ -2,7 +2,7 @@ from . import util
 import logging
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from bot.database import categories as cat
+from database import categories as cat
 from telegram.ext import (
     Application,
     CommandHandler,
